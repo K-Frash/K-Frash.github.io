f1a0a7041b1eb6d85f1fd23f1dea4057fe6ac05c
Kris Frasheri <krisfrasheri@gmail.com>
2025-06-24 13:59:26 -0400

    Updates

---
+++ assets/lesson1-gXBN0Bg0.js
@@ -0,0 +1,54 @@
+const e=`# Course Description
+This course is an introduction to contemporary user interface implementation concepts, including event abstraction, graphical components, layout, feedback, testing, accessibility, and architectures to develop user interfaces. One or more types of interface toolkit paradigms are considered.
+
+
+## Learning Outcomes
+At the end of the course, students should be able to:
+* Explain architectural and algorithmic details underlying current user interfaces and user interface toolkits.
+* Describe key aspects of user interfaces, such as user input, event-driven architecture, asynchronous events, etc.
+* Implement user interfaces in a browser with Typescript: (1) using the HTML Canvas; (2) using HTML elements and DOM manipulation, and (3) using a modern UI web development framework.
+
+
+
+# Course Notes
+
+## Lessons
+Below are all the slides I created and presented in instructing my offering of CS349 (with content support from [Dr. Dan Vogel](https://www.nonsequitoria.com/)). Note, these slides were paired in conjunction with in-class code demos to illustrate topics being presented:
+
+* Simplekit, Canvas and TypeScript
+  1. [Lecture 00: Introduction to User Interfaces](https://drive.google.com/file/d/1AI5TZ85CVeU8usbZJMnwIq_BcJlcfiKG/view?usp=drive_link)
+  1. [Lecture 01: Web Application Technologies](https://drive.google.com/file/d/1aMNeSNtTxtcuQdKQdsh9rAnxT65lPCkk/view?usp=drive_link)
+  1. [Lecture 02: Introduction to JavaScript](https://drive.google.com/file/d/1BtT1v_a3uGSV7PfVARwZrwxmhId7f8jw/view?usp=drive_link)
+  1. [Lecture 03: Introduction to TypeScript](https://drive.google.com/file/d/18iEuyg79w5wPVrBMGXpKzgUPr-F9OP7W/view?usp=drive_link)
+  1. [Lecture 04: Drawing](https://drive.google.com/file/d/11ajLff-H9x0fVwsKIq1PGbWvjgTmK71X/view?usp=drive_link)
+  1. [Lecture 05: Input Events](https://drive.google.com/file/d/1EnWabvfjKO43cluDLc2CWmQ4syc2q85l/view?usp=drive_link)
+  1. [Lecture 06: Hit-Testing](https://drive.google.com/file/d/1IPxseUW9vSHDF8QbAP-l8rDVnb4LfdGx/view?usp=drive_link)
+  1. [Lecture 07: Animation](https://drive.google.com/file/d/1GiT5aqFMl9fvs_py5X1rRyygyrqzBoD5/view?usp=drive_link)
+
+* Widgets
+  1. [Lecture 08: Widgets](https://drive.google.com/file/d/12nHv7NVT2kki1OVLwhhag61szKVbEPpB/view?usp=drive_link)
+  1. [Lecture 09: Event Dispatch](https://drive.google.com/file/d/1dkDqwr7Affano5brSxPtOR4B7YwrRcye/view?usp=drive_link)
+  1. [Lecture 10: Layout](https://drive.google.com/file/d/1Pjd3QHkjDybjtLkJyB_kDw0bMzf2ocIE/view?usp=drive_link)
+  1. [Lecture 11: Model View Controller (MVC)](https://drive.google.com/file/d/1DF_L3GhQ93HXdgIsnOFwWnTL3Ty1PTCn/view?usp=drive_link)
+
+* Vanilla HTML, CSS and TypeScript
+  1. [Lecture 12: HTML CSS](https://drive.google.com/file/d/1-1HKH_VdV3dcZEAKhRUvSs7_No2vfUIq/view?usp=drive_link)
+  1. [Lecture 13: Visual Design](https://drive.google.com/file/d/18s3EIyyZ7mImXz9_xGEjRBH-YJO68NJJ/view?usp=drive_link)
+  1. [Lecture 14: Text](https://drive.google.com/file/d/18FkovmZ3fcsFy5gz7pInR7DSLhZF6pXb/view?usp=drive_link)
+  1. [Lecture 15: Undo](https://drive.google.com/file/d/15qgDFlN3gVtkhX1P2dPPHc7I-SpM5Su8/view?usp=drive_link)
+
+* Preact
+  1. [Lecture 16: Asynchronous](https://drive.google.com/file/d/1-pke8lUeOyMdb0eK9XbHKJmA0cQJ3_jM/view?usp=drive_link)
+  1. [Lecture 17: Declarative](https://drive.google.com/file/d/1MpX23QcaqAX6tHrNjcuos3ZYTIHDw84f/view?usp=drive_link)
+  1. [Lecture 18: Reactive](https://drive.google.com/file/d/1UBEs4P3BAWRjIyO3cADjiJvLKO8aI0IX/view?usp=drive_link)
+  1. [Lecture 19: Effects](https://drive.google.com/file/d/1AvF_0J9mYY48Js74WexLq02-baplttOu/view?usp=drive_link)
+  1. [Lecture 20: Direct Manipulation](https://drive.google.com/file/d/1AtqghCzNE4R8hFxKBTtvGhu9h-m497mb/view?usp=drive_link)
+  1. [Lecture 21: Transformable](https://drive.google.com/file/d/1Q3pTwsfCJjy7gt1UcL9l0pfLjyshJsfQ/view?usp=drive_link)
+  1. [Lecture 22: Accessibility](https://drive.google.com/file/d/1tz1xjEAWAYQHVcsTHpiLao20yF61QVK4/view?usp=drive_link)
+
+
+## Extras
+
+### Final Review
+* [Broad slidshow encompassing final exam topics alongside select examples](https://drive.google.com/file/d/15rAsv2Pmk9G6j5RIk8WXZDIQbC44RjFx/view?usp=drive_link)
+`;export{e as default};
